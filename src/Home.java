24f06c263ea90240a968c263ad8ebb2ca147859a
abesquiv <abesquiv@us.ibm.com>
2015-06-09 10:01:54 -0400

    added home page with instructions

---
+++ src/Home.java
@@ -0,0 +1,19 @@
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Response;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+ 
+@Path("")
+public class Home {
+  @GET
+  @Produces("application/json")
+  public Response convertFtoC() throws JSONException {
+    String result = "use /ctofservice/35 to convert 35 Celsius to Fahrenheit \n\n other services include \nftocservice\nctokservice";
+    return Response.status(200).entity(result).build();
+  }
+
+}
